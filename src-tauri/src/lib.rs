b88e0d035f8d1209067fb2b1b52b7896caef2fc1
on9au <151502370+on9au@users.noreply.github.com>
2025-01-31 17:09:46 +1100

    add is_name_cringe command and update greet function for name validation

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -4,11 +4,16 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+#[tauri::command]
+fn is_name_cringe(name: &str) -> bool {
+    name.to_lowercase().trim_end().contains("cameron")
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, is_name_cringe])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,34 @@ import { invoke } from "@tauri-apps/api/core";
 let greetInputEl: HTMLInputElement | null;
 let greetMsgEl: HTMLElement | null;
 
-async function greet() {
+async function funky() {
   if (greetMsgEl && greetInputEl) {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    greetMsgEl.textContent = await invoke("greet", {
-      name: greetInputEl.value,
-    });
+    const name = greetInputEl.value;
+    const cringe = await is_name_cringe(name);
+    if (cringe) {
+      greetMsgEl.textContent = "Cringe name detected. Please kill yourself.";
+    } else {
+      greetMsgEl.textContent = await greet(name)
+    }
   }
+}
+
+async function greet(name: string): Promise<string> {
+  // if (greetMsgEl && greetInputEl) {
+  // // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+  // greetMsgEl.textContent = await invoke("greet", {
+  //   name: greetInputEl.value,
+  // });
+  return await invoke("greet", {
+    name: name,
+  });
+  // }
+}
+
+async function is_name_cringe(name: string): Promise<boolean> {
+  return await invoke("is_name_cringe", {
+    name: name,
+  });
 }
 
 window.addEventListener("DOMContentLoaded", () => {
@@ -17,6 +38,6 @@ window.addEventListener("DOMContentLoaded", () => {
   greetMsgEl = document.querySelector("#greet-msg");
   document.querySelector("#greet-form")?.addEventListener("submit", (e) => {
     e.preventDefault();
-    greet();
+    funky();
   });
 });
