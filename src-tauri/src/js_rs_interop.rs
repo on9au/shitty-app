0ef5bb3a1c39d8c59ea96a507fbac8ae4e445c00
on9au <151502370+on9au@users.noreply.github.com>
2025-01-31 18:43:39 +1100

    add async message passing functionality with js2rs and rs2js commands

---
+++ src-tauri/src/js_rs_interop.rs
@@ -0,0 +1,57 @@
+use std::ops::{Deref, DerefMut};
+
+use tracing::info;
+
+/// Async Process Input Transmitter
+///
+/// Main Thread -> Tokio
+pub struct AsyncProcInputTx {
+    inner: tokio::sync::Mutex<tokio::sync::mpsc::Sender<String>>,
+}
+
+impl AsyncProcInputTx {
+    pub fn new(tx: tokio::sync::mpsc::Sender<String>) -> Self {
+        Self {
+            inner: tokio::sync::Mutex::new(tx),
+        }
+    }
+}
+
+impl Deref for AsyncProcInputTx {
+    type Target = tokio::sync::Mutex<tokio::sync::mpsc::Sender<String>>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.inner
+    }
+}
+
+impl DerefMut for AsyncProcInputTx {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.inner
+    }
+}
+
+/// This function is called from JavaScript to send a message to Rust-Tokio.
+#[tauri::command]
+pub async fn js2rs(
+    message: String,
+    state: tauri::State<'_, AsyncProcInputTx>,
+) -> Result<(), String> {
+    info!(?message, "js2rs");
+    let async_process_input_tx = state.lock().await;
+    async_process_input_tx
+        .send(message)
+        .await
+        .map_err(|e| e.to_string())
+}
+
+/// This function is called from Rust-Tokio to send a message to JavaScript.
+pub fn rs2js<R: tauri::Runtime>(
+    app_handle: &(impl tauri::Manager<R> + tauri::Emitter<R>),
+    message: String,
+) {
+    info!(?message, "rs2js");
+    app_handle
+        .emit("rs2js", format!("{{\"message\": \"{}\"}}", message))
+        .expect("failed to emit event");
+}
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,7 @@
+use js_rs_interop::{rs2js, AsyncProcInputTx};
+use tokio::sync::mpsc;
+
+pub mod js_rs_interop;
 pub mod protocol;
 
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
@@ -13,9 +17,42 @@ fn is_name_cringe(name: &str) -> bool {
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    // Create message passing channels
+    let (async_proc_input_tx, async_proc_input_rx) = tokio::sync::mpsc::channel(1);
+    let (async_proc_output_tx, mut async_proc_output_rx) = tokio::sync::mpsc::channel(1);
+
     tauri::Builder::default()
+        .manage(AsyncProcInputTx::new(async_proc_input_tx))
+        .setup(|app| {
+            tauri::async_runtime::spawn(async move {
+                async_process_model(async_proc_input_rx, async_proc_output_tx).await
+            });
+
+            let app_handle = app.handle().clone();
+            tauri::async_runtime::spawn(async move {
+                loop {
+                    if let Some(output) = async_proc_output_rx.recv().await {
+                        rs2js(&app_handle, output);
+                    }
+                }
+            });
+
+            Ok(())
+        })
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet, is_name_cringe])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+async fn async_process_model(
+    mut input_rx: mpsc::Receiver<String>,
+    output_tx: mpsc::Sender<String>,
+) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    while let Some(input) = input_rx.recv().await {
+        let output = input;
+        output_tx.send(output).await?;
+    }
+
+    Ok(())
 }
