0956c0c90f8658bf8e05c0eb12a00166bffe540e
on9au <151502370+on9au@users.noreply.github.com>
2025-02-02 13:17:31 +1100

    feat: enhance connection handling by integrating ECDSA connection info in response

--- src-tauri/src/backend/frontend_handlers/connection_request_response.rs
+++ src-tauri/src/backend/frontend_handlers/connection_request_response.rs
@@ -4,7 +4,7 @@ use crate::{
     backend::{
         frontend_manager::FrontendManager,
         peer_manager::PeerState,
-        protocol::{ConnectionPermit, ConnectionResponse, Message},
+        protocol::{ConnectionPermit, ConnectionResponse, EcdsaConnectionInfo, Message},
     },
     js_api::frontend_event::ConnectionRequestResponse,
 };
@@ -26,28 +26,31 @@ impl FrontendManager {
             if connection_request_response.accept {
                 // Connection accepted, change state to `Authenticated` and send a `ConnectResponse` with `Permit` message
                 // peer.state = PeerState::Authenticated;
-                match &peer.state {
-                    PeerState::Connected { peer_info } => {
-                        let peer_info = peer_info.as_ref().expect("Peer info was not set when handling the connection request response???");
-                        let connection_response = ConnectionResponse {
-                            permit: ConnectionPermit::Permit {
-                                identitiy: todo!("Setup peer identity"),
+                if let PeerState::Connected { peer_info } = &peer.state {
+                    let peer_info = peer_info.as_ref().expect(
+                        "Peer info was not set when handling the connection request response???",
+                    );
+                    let connection_response = ConnectionResponse {
+                        permit: ConnectionPermit::Permit {
+                            identitiy: EcdsaConnectionInfo {
+                                public_key: vec![], // TODO: Implement
+                                signature: vec![],  // TODO: Implement
+                                nonce: vec![],      // TODO: Implement
                             },
-                            message: connection_request_response.message.clone(),
-                        };
+                        },
+                        message: connection_request_response.message.clone(),
+                    };
 
-                        // Update state to `Authenticated`
-                        peer.state = PeerState::Authenticated {
-                            peer_info: peer_info.clone(),
-                        };
+                    // Update state to `Authenticated`
+                    peer.state = PeerState::Authenticated {
+                        peer_info: peer_info.clone(),
+                    };
 
-                        // Send the connection response
-                        peer.tx
-                            .send(Message::ConnectResponse(connection_response))
-                            .await
-                            .expect("Failed to send ConnectResponse message to the peer");
-                    }
-                    _ => {}
+                    // Send the connection response
+                    peer.tx
+                        .send(Message::ConnectResponse(connection_response))
+                        .await
+                        .expect("Failed to send ConnectResponse message to the peer");
                 }
             } else {
                 // Connection rejected, send a `ConnectResponse` with `Deny` message
